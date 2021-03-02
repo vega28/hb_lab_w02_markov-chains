c6da54863a476c3518f19a0ba19fef01842c0dee
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-03-02 20:50:59 +0000

    Completed the function to read in the file and make change func
    Co-authored-by: Kristen Sutherland <kristen1286@gmail.com>
    Co-authored-by: Kelsi Flatland <kelsi.flatland@gmail.com>

--- markov.py
+++ markov.py
@@ -11,8 +11,11 @@ def open_and_read_file(file_path):
     """
 
     # your code goes here
+    file_content = open(file_path).read()
+    
 
-    return 'Contents of your file as one long string'
+    return file_content
+
 
 
 def make_chains(text_string):
@@ -39,11 +42,29 @@ def make_chains(text_string):
         >>> chains[('there','juanita')]
         [None]
     """
+    # chains = { ('hi', 'there') : ['mary', 'juanita'] }
 
     chains = {}
 
     # your code goes here
-
+    # splitting our string thats passed in .split() with whitespace
+    words = text_string.split()
+    # store that in a list so we can iterate. for loop?
+    # tracking index using enumerate or range/len
+    # as we iterate add to our markov dict. we also want to create a tuple(key) and
+    for i in range(len(words)-2): # deal with edge case @ end of list using -2
+        # my_dict[key] = value
+        if (words[i],words[i+1]) not in chains:
+            chains[(words[i],words[i+1])] = [words[i+2]]
+            # append to values list if the key-value pair exists already
+        else:
+            chains[(words[i],words[i+1])].append(words[i+2])
+            # else create the key-value pair
+        
+            # values.append(' the word after that') 
+    # add our words to the list as the value. 
+    # check out keys to see if they exist
+    print(chains)
     return chains
 
 
